7b3af89636e6f64a612c14a79c704e66a67cb97d
xpressinside <43053289+xpressinside@users.noreply.github.com>
2024-11-14 00:37:38 +1100

    Add files via upload

---
+++ s21_cat/generate_table raw.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i != 256; ++i) {
+        if (i != 0 && !(i % 10))
+            printf("\n");
+        else
+            printf(" ");
+        printf("\"");
+        switch (i)
+        {
+            case '\n':
+            printf("\\n");
+            break; case '\t':
+            printf("\\t");
+            break; case '\v':
+            printf("\\v");
+            break; case '\b':
+            printf("\\b");
+            break; case '\r':
+            printf("\\r");     
+            break; case '\'':
+            printf("\\'");
+            break; case '\"':
+            printf("\\\""); 
+            break; case '\\':
+            printf("\\");                                                                                
+            default:
+            if (i < 32)
+                printf("\\0%o", i);
+            else if (i > '~')
+                printf("\\x%c", i);
+        }
+
+    }
+}
